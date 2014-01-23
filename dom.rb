d59c53171fc4a948bfcf9187b9431f4fa7887197
Artjom Juferov <derevjaginti@gmail.com>
2014-01-23 17:32:12 +0300

    must be acomplised under #notready

--- dom.rb
+++ dom.rb
@@ -18,27 +18,29 @@ def create_product_description(body)
   #obj = storeformatpieces(body, obj)
   #obj = related(body, obj)
   #obj = numberOfPackages(body, obj)
-  obj = packageInfo(body, obj)
+  #obj = packageInfo(body, obj)
+  #obj = packageInfo(body, obj)
+  obj = breadCrumbs(body, obj)
+  #obj = moreModel(body, obj)
+  #obj = links(body,obj)
+  
   File.open("test.json", 'w') { |f| f.write(obj.to_json) }
 end
 
-#--------------
-# top
 def simple(id, body, obj)
   result = body.scan(/<div id\s*="#{id}".*?>(.*?)<\/div>/m)
-  if result[0] != nil and result[0][0] != nil
-    obj[id] = result[0][0].to_s.gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
+  if result.any?
+    obj[id] = result[0][0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
   else 
     obj[id] = ''
   end
   return obj
 end
 
-# where  подробнее
 def salesArg(body, obj)
   result = body.scan(/<div id="salesArg".*?>(.*?)<a/m)
-  if result[0] != nil and result[0][0] != nil
-    obj["salesArg"] = result[0][0].to_s.gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
+  if result.any?
+    obj["salesArg"] = result[0][0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
   else 
     obj["salesArg"] = ''
   end
@@ -47,14 +49,11 @@ end
 
 def pdfs(body, obj)
   result = body.scan(/<div class="colAttachment">\s+<a href="(.*?)" target="_blank"/m)
-  arr = []
-  result.each do |el|
-    if el[0] != nil
-      arr.push(el[0].to_s.force_encoding('UTF-8'))
+  if result.any?   
+    result.each do |element|
+      element = element[0].force_encoding('UTF-8')
     end
-  end
-  if arr != nil
-    obj["pdfs"] = arr
+    obj["pdfs"] = result
   else 
     obj["pdfs"] = ''
   end
@@ -63,10 +62,24 @@ end
 
 def images(body, obj)
   result = body.scan(/"images":{"large":\[(.*?)\]/m)
+  if result.any?
+    result[0][0].split(/","/).each do |elemenet|
+      result.push(elemenet.to_s.gsub(/\t|\n|\r|"|\\/, "").force_encoding('UTF-8'))
+    end
+    obj["images"] = result
+  else 
+    obj["images"] = ''
+  end
+  return obj
+end
+
+=begin
+def images(body, obj)
+  result = body.scan(/"images":{"large":\[(.*?)\]/m)
   arr = [] 
-  if result[0] != nil and result[0][0] != nil
-    result[0][0].split(/","/).each do |img|
-      arr.push(img.to_s.gsub(/\t|\n|\r|"|\\/, "").force_encoding('UTF-8'))
+  if result.any?
+    result[0][0].split(/","/).each do |elemenet|
+      arr.push(elemenet.to_s.gsub(/\t|\n|\r|"|\\/, "").force_encoding('UTF-8'))
     end
     obj["images"] = arr
   else 
@@ -74,22 +87,22 @@ def images(body, obj)
   end
   return obj
 end
+=end
 
 def price1(body, obj)
   result = body.scan(/<span id="price1".*?>(.*?)\./m)
-  if result[0] != nil and result[0][0] != nil
-    obj["price1"] = result[0][0].to_s.gsub(/\D/, "").force_encoding('UTF-8')
+  if result.any?
+    obj["price1"] = result[0][0].gsub(/\D/, "").force_encoding('UTF-8')
   else 
     obj["price1"] = ''
   end
   return obj
 end
 
-
 def storeformatpieces(body, obj)
   result = body.scan(/<span id="storeformatpieces".*?>(.*?)<\/span>/m)
-  if result[0] != nil and result[0][0] != nil
-    obj["storeformatpieces"] = result[0][0].to_s.gsub(/\D/, "").force_encoding('UTF-8')
+  if result.any?
+    obj["storeformatpieces"] = result[0][0].gsub(/\D/, "").force_encoding('UTF-8')
   else 
     obj["storeformatpieces"] = ''
   end
@@ -98,8 +111,8 @@ end
 
 def related(body, obj)
   result = body.scan(/MAY_BE_COMPLETED_WITH":\[(.*?)\]/m)
-  if result[0] != nil and result[0][0] != nil
-    obj["related"] = result[0][0].to_s.gsub(/"/, "").force_encoding('UTF-8')
+  if result.any?
+    obj["related"] = result[0][0].gsub(/"/, "").force_encoding('UTF-8')
   else 
     obj["related"] = ''
   end
@@ -108,8 +121,8 @@ end
 
 def numberOfPackages(body, obj)
   result = body.scan(/<span id="numberOfPackages">(.*?)<\/span>/m)
-  if result[0] != nil and result[0][0] != nil
-    obj["numberOfPackages"] = result[0][0].to_s.gsub(/\D/, "").force_encoding('UTF-8')
+  if result.any?
+    obj["numberOfPackages"] = result[0][0].gsub(/\D/, "").force_encoding('UTF-8')
   else 
     obj["numberOfPackages"] = ''
   end
@@ -117,22 +130,75 @@ def numberOfPackages(body, obj)
 end
 
 def packageInfo(body, obj)
-  result = body.scan(/<div id="packageInfo">.*?<div class="texts">(.*?)<\/div>/m)
-  if result[0] != nil and result[0][0] != nil
-    obj["packageInfo"] = result[0][0].to_s.gsub(/\D/, "").force_encoding('UTF-8')
+  result = body.scan(/<span id="numberOfPackages">.*?<\/span>.*? class="texts".*?>(.*?)<\/div>/m)
+  if result.any?
+    obj["packageInfo"] = result[0][0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
   else 
     obj["packageInfo"] = ''
   end
   return obj
 end
 
+#not ready
+def breadCrumbs(body, obj)
+  result = body.scan(/<span id="numberOfPackages">.*?<\/span>.*? class="texts".*?>(.*?)<\/div>/m)
+  if result.any?
+    obj["packageInfo"] = result[0][0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
+  else 
+    obj["packageInfo"] = ''
+  end
+  return obj
+end
 
+#not ready
+def moreModel(body, obj)
+
+end
+
+def links(body, obj)
+  result = body.scan(/<div id="subDivDropDown\d".*?>(.*?)<\/select>/m)
+  arr = Hash.new
+  if result.any? 
+    result.each do |elemenet|
+      elemenet[0] = elemenet[0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')  
+      name = element[0].scan(/<label id="subDivDropDownLbl".*?>(.*?)<\/label>/m)
+      if name.any?
+        arr["#{name}"]
+      end
+    end
+    obj["links"] = result
+  else 
+    obj["links"] = ''
+  end
+  return obj
+end
+
+=begin
+#not ready
+def links(body, obj)
+  result = body.scan(/<div id="subDivDropDown\d".*?>(.*?)<\/select>/m)
+  arr = Hash.new
+  if result.any? 
+    result.each do |elemenet|
+      elemenet[0] = elemenet[0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')  
+      name = element[0].scan(/<label id="subDivDropDownLbl".*?>(.*?)<\/label>/m)
+      if name.any?
+        arr["#{name}"]
+      end
+    end
+    obj["links"] = result
+  else 
+    obj["links"] = ''
+  end
+  return obj
+end
+=end
 
 a = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
 
 
-a.get('http://www.ikea.com/ru/ru/catalog/products/30079365/') do |page_product|          
+a.get('http://www.ikea.com/ru/ru/catalog/products/S19836967/') do |page_product|          
   create_product_description(page_product.body)
 end
