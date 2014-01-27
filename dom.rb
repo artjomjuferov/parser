2fb2f4b65a7e7e688629e9e16fddb1157e7a2cb4
Artjom Juferov <derevjaginti@gmail.com>
2014-01-27 17:43:21 +0300

    donegit add dom.rb

--- dom.rb
+++ dom.rb
@@ -8,21 +8,20 @@ FUNCTIONS = ['simple', 'salesArg', 'pdfs', 'images', 'price1', 'storeformatpiece
 
 def create_product_description(body)
   obj = Hash.new
-  #IDS_SIMPLE.each do |id_name|
-  #  obj = simple(id_name,body, obj)     
-  #end 
-  #obj = salesArg(body, obj)
-  #obj = pdfs(body, obj)
-  #obj = images(body, obj)
-  #obj = price1(body, obj)
-  #obj = storeformatpieces(body, obj)
-  #obj = related(body, obj)
-  #obj = numberOfPackages(body, obj)
-  #obj = packageInfo(body, obj)
-  #obj = packageInfo(body, obj)
+  IDS_SIMPLE.each do |id_name|
+    obj = simple(id_name,body, obj)     
+  end 
+  obj = salesArg(body, obj)
+  obj = pdfs(body, obj)
+  obj = images(body, obj)
+  obj = price1(body, obj)
+  obj = storeformatpieces(body, obj)
+  obj = related(body, obj)
+  obj = numberOfPackages(body, obj)
+  obj = packageInfo(body, obj)
+  obj = packageInfo(body, obj)
   obj = breadCrumbs(body, obj)
-  #obj = moreModel(body, obj)
-  #obj = links(body,obj)
+  obj = links(body,obj)
   
   File.open("test.json", 'w') { |f| f.write(obj.to_json) }
 end
@@ -73,21 +72,6 @@ def images(body, obj)
   return obj
 end
 
-=begin
-def images(body, obj)
-  result = body.scan(/"images":{"large":\[(.*?)\]/m)
-  arr = [] 
-  if result.any?
-    result[0][0].split(/","/).each do |elemenet|
-      arr.push(elemenet.to_s.gsub(/\t|\n|\r|"|\\/, "").force_encoding('UTF-8'))
-    end
-    obj["images"] = arr
-  else 
-    obj["images"] = ''
-  end
-  return obj
-end
-=end
 
 def price1(body, obj)
   result = body.scan(/<span id="price1".*?>(.*?)\./m)
@@ -139,66 +123,74 @@ def packageInfo(body, obj)
   return obj
 end
 
-#not ready
+
 def breadCrumbs(body, obj)
-  result = body.scan(/<span id="numberOfPackages">.*?<\/span>.*? class="texts".*?>(.*?)<\/div>/m)
+  result = body.scan(/<ul id="breadCrumbs">(.*?)<\/ul>/m)
   if result.any?
-    obj["packageInfo"] = result[0][0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')
-  else 
-    obj["packageInfo"] = ''
+    tmp = result[0][0].scan(/href="\/ru\/ru\/catalog\/categories\/departments\/(.*?)\/".?>(.*?)<\/a>/m)
+    p tmp
+    i = 0 
+    tmp.each do |el|
+      i += 1
+      el[1] = el[1].force_encoding('UTF-8')
+      obj["breadCrumbs#{i}"] = el
+    end
   end
+  p obj
   return obj
 end
 
-#not ready
-def moreModel(body, obj)
 
+def find_last_suitable(value, body)
+  tmp = body.scan(/"partNumber"(.*?"catEntryId":"#{value}")/)
+  if tmp.any?
+    return find_last_suitable(value, tmp[0][0])
+  else 
+    return body
+  end
 end
 
 def links(body, obj)
-  result = body.scan(/<div id="subDivDropDown\d".*?>(.*?)<\/select>/m)
-  arr = Hash.new
-  if result.any? 
-    result.each do |elemenet|
-      elemenet[0] = elemenet[0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')  
-      name = element[0].scan(/<label id="subDivDropDownLbl".*?>(.*?)<\/label>/m)
-      if name.any?
-        arr["#{name}"]
+  result = body.scan(/<div id="subDivDropDown(\d)".*?>(.*?)<\/select>/m)
+  if !result.any?
+    return obj
+  end
+  i = 0 
+  result.each do |element|
+    links = Hash.new
+    array_of_atr = []
+    tmp = body.scan(/<label id="subDivDropDownLbl#{element[0]}.*?for="drop(.*?)"/m)
+    if tmp.any? and element.any?
+      options = element[1].scan(/<option value="(.*?)"/m)
+      options.each do |option|
+        option_name = element[1].scan(/<option value="#{option[0]}".*?>(.*?)<\/option>/m)
+        if option_name.any?
+          option_name = option_name[0][0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')  
+        else
+          option_name = ''
+        end
+        option[0].split(/;/).each do |value|
+          link = Hash.new
+          id = find_last_suitable(value, body).scan(/:"(.*?)","attachments"/)
+          if id.any? 
+            link["#{tmp[0][0].force_encoding('UTF-8')}"] = option_name 
+            links["#{id[0][0]}"] = link
+          end 
+        end
       end
+      obj["links#{i}"] = links
     end
-    obj["links"] = result
-  else 
-    obj["links"] = ''
   end
   return obj
 end
 
-=begin
-#not ready
-def links(body, obj)
-  result = body.scan(/<div id="subDivDropDown\d".*?>(.*?)<\/select>/m)
-  arr = Hash.new
-  if result.any? 
-    result.each do |elemenet|
-      elemenet[0] = elemenet[0].gsub(/\t|\n|\r/, "").force_encoding('UTF-8')  
-      name = element[0].scan(/<label id="subDivDropDownLbl".*?>(.*?)<\/label>/m)
-      if name.any?
-        arr["#{name}"]
-      end
-    end
-    obj["links"] = result
-  else 
-    obj["links"] = ''
-  end
-  return obj
-end
-=end
 
-a = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-}
+#a = Mechanize.new { |agent|
+#  agent.user_agent_alias = 'Mac Safari'
+#}
 
+p `ls bathroom/10555/00074185d`.empty?
 
-a.get('http://www.ikea.com/ru/ru/catalog/products/S19836967/') do |page_product|          
-  create_product_description(page_product.body)
-end
+#a.get('http://www.ikea.com/ru/ru/catalog/products/S29836976/') do |page_product|          
+#  create_product_description(page_product.body)
+#end
